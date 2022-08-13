270df44b37fd817aabf9ef8dbb7ea7389c9bfdc5
krauserhuang <crescentdence@gmail.com>
2022-08-13 11:15:57 +0800

    Initial Commit

---
+++ Face ID Touch ID Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Face ID Touch ID Demo
+//
+//  Created by Tai Chin Huang on 2022/8/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
